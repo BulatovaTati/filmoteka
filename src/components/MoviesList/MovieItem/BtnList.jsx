b57b36dc2860e5e001e2e065385dde5ad83e0816
BulatovaTati <kohodatat@gmail.com>
2025-01-25 19:50:30 +0200

    add flip card

--- src/components/MoviesList/MovieItem/BtnList.jsx
+++ src/components/MoviesList/MovieItem/BtnList.jsx
@@ -1,15 +1,19 @@
 import s from './BtnList.module.css';
 
 const BtnList = () => {
+  const handleButtonClick = e => {
+    e.stopPropagation();
+  };
+
   return (
     <ul className={s.btn_list}>
       <li>
-        <button type="button" className={s.button_modal_btn}>
+        <button type="button" className={s.button_modal_btn} onClick={handleButtonClick}>
           add to Watched
         </button>
       </li>
       <li>
-        <button type="button" className={s.button_modal_btn}>
+        <button type="button" className={s.button_modal_btn} onClick={handleButtonClick}>
           add to queue
         </button>
       </li>
--- src/components/MoviesList/MovieItem/MovieItem.jsx
+++ src/components/MoviesList/MovieItem/MovieItem.jsx
@@ -5,6 +5,7 @@ import MovieCardInfo from './MovieCardInfo';
 import MovieOverview from './MovieOverview';
 import { selectGenres } from '../../../redux/movies/selectors';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const MovieItem = ({ movie }) => {
   const {
@@ -17,6 +18,7 @@ const MovieItem = ({ movie }) => {
     name,
     overview,
   } = movie;
+  const [isFlipped, setIsFlipped] = useState(false);
 
   const genres = useSelector(selectGenres);
 
@@ -25,9 +27,13 @@ const MovieItem = ({ movie }) => {
     return genre ? genre.name : 'Unknown';
   });
 
+  const handleCardFlip = () => {
+    setIsFlipped(prev => !prev);
+  };
+
   return (
-    <li className={s.card__item}>
-      <Link className={s.gallery_art}>
+    <li className={`${s.card__item} ${isFlipped ? s['is-flipped'] : ''}`} onClick={handleCardFlip}>
+      <div className={s.card__item_front}>
         <MoviePoster poster_path={poster_path} title={title} />
         <MovieCardInfo
           genreNames={genreNames}
@@ -37,8 +43,10 @@ const MovieItem = ({ movie }) => {
           release_date={release_date}
           first_air_date={first_air_date}
         />
+      </div>
+      <div className={s.card__item_back}>
         <MovieOverview overview={overview} />
-      </Link>
+      </div>
     </li>
   );
 };
