abc4dc8a0325cc2eae51732a35310348b3b67aef
BulatovaTati <kohodatat@gmail.com>
2025-01-28 20:23:08 +0200

    add corrections

--- src/components/Modals/RegistrationModal.jsx
+++ src/components/Modals/RegistrationModal.jsx
@@ -1,27 +1,27 @@
-import Modal from 'react-modal';
-import { MdCancel } from 'react-icons/md';
+// import Modal from 'react-modal';
+// import { MdCancel } from 'react-icons/md';
 
-import RegistrationForm from '../Auth/RegistrationForm/RegistrationForm';
-import s from './RegistrationModal.module.css';
+// import RegistrationForm from '../Auth/RegistrationForm/RegistrationForm';
+// import s from './RegistrationModal.module.css';
 
-Modal.setAppElement('#root');
+// Modal.setAppElement('#root');
 
-const RegistrationModal = ({ isOpen, onClose }) => {
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      aria-modal="true"
-      className={s.modal}
-      overlayClassName={s.overlay}
-      shouldCloseOnOverlayClick={true}
-    >
-      <button onClick={onClose} className={s.cancelBtn}>
-        <MdCancel />
-      </button>
-      <RegistrationForm />
-    </Modal>
-  );
-};
+// const RegistrationModal = ({ isOpen, onClose }) => {
+//   return (
+//     <Modal
+//       isOpen={isOpen}
+//       onRequestClose={onClose}
+//       aria-modal="true"
+//       className={s.modal}
+//       overlayClassName={s.overlay}
+//       shouldCloseOnOverlayClick={true}
+//     >
+//       <button onClick={onClose} className={s.cancelBtn}>
+//         <MdCancel />
+//       </button>
+//       <RegistrationForm />
+//     </Modal>
+//   );
+// };
 
-export default RegistrationModal;
+// export default RegistrationModal;
