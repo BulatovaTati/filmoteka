818d54411e587e8ddf590055655ddc8f5237eee5
BulatovaTati <kohodatat@gmail.com>
2025-02-27 13:59:57 +0200

    new Credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.API_KEY,
   authDomain: 'filmoteka-fe44d.firebaseapp.com',
   projectId: 'filmoteka-fe44d',
   storageBucket: 'filmoteka-fe44d.appspot.com',
